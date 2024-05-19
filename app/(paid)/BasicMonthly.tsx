cca8c979ec3bbab89473510e22e70bcdd4b52502
MOmar <momar.ttv@gmail.com>
2024-05-18 22:46:46 -0400

    Version 0.3

--- app/(paid)/BasicMonthly.tsx
+++ app/(paid)/BasicMonthly.tsx
@@ -2,11 +2,18 @@ import { View, Text, StyleSheet } from "react-native";
 export default function BasicMonthly() {
     return (
         <View>
-            <Text>Basic Monthly</Text>
+            <Text style={styles.title}>Basic Monthly Plan Package</Text>
+
         </View>
     )
 }
 
 const styles = StyleSheet.create({
+    title: {
+        fontSize: 20,
+        fontWeight: "bold",
+        color: "#000000",
+        textAlign: "center",
+    }
     
 })
